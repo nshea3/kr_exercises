7ffe92393631d2e53fb2424dfc7b51edaf7195c3
Nick Shea <nshea@email.arizona.edu>
2019-06-30 12:57:22 -0700

    Updated with word count

--- ch1/exercise_1_13.c
+++ ch1/exercise_1_13.c
@@ -2,26 +2,25 @@
 
 main()
 {
-    int c, i, j, nwhite, nother;
-    int ndigit[10];
+    int c, i, j, nwhite, nother, num_inword;
+    int nletters[30];
 
     nwhite = nother = 0;
-    for(i = 0; i < 10; ++i){
-        ndigit[i] = 0;
+    for(i = 0; i < 30; ++i){
+        nletters[i] = 0;
     }
 
     while ((c = getchar()) != EOF){
-        if (c >= '0' && c <= '9'){
-            ++ndigit[c - '0'];
-        } else if (c == ' ' || c == '\n' || c == '\t') {
-            ++nwhite;
+        if (c >= 'a' && c <= 'z'){
+            ++num_inword;
         } else {
-            ++nother;
+            ++nletters[num_inword];
+            num_inword = 0;
         }
     }
 
     for (i = 0; i < 10; ++i){
-        for (j = 0; j < ndigit[i]; ++j){
+        for (j = 0; j < nletters[i]; ++j){
             printf("-");
         }
         printf("\n");
